2aed177de69071c3a3bb48e10896e5bc85d8f4f0
Soumya <soumyasataiah@gmail.com>
2025-02-11 19:14:25 +0530

    Added post request

--- index.js
+++ index.js
@@ -8,14 +8,32 @@ app.use(cors({
     origin: ["http://localhost:5173","https://client-sepia-six.vercel.app"]
 }));
 
+app.use(express.json());
 
-app.get( "/soumi" , ( req , res ) => {
-    res.send("Hello,Soumya Here :)");
-})
+mongoose
+  .connect("mongodb+srv://soumyasataiah:VVy9o0nBKCfabwSK@cluster0.1shwg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") 
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
+
+  const userSchema = new mongoose.Schema({
+    name: String,
+    age: Number
+  });
+  
+  const User = mongoose.model("User", userSchema);
+
+  app.get( "/soumi" , async ( req , res ) => {
+    const result= await User.find()
+     res.json(result);
+});
+
+app.post( "/soumi" , async ( req , res ) => {
+     const { name,age }= req.body;
+     const newUser=new User({name,age});
+     await newUser.save();
+     res.json(newUser);
+});
 
 app.listen(port,()=>{
     console.log(`Listening to the port ${port}`);
 })
-
-
-
